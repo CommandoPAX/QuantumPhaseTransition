61a1d707f08e18dbf6180dd74d73b66b1f596be1
CommandoPAX <bruant.theo@gmail.com>
2024-10-15 11:05:21 +0200

    sdsdza

--- qpt.jl
+++ qpt.jl
@@ -10,26 +10,27 @@ using Random
 
 let 
     N = 100
-    sites = siteinds("Arg",N)
+    sites = siteinds("Qudit", N)
     U = 1
     J = 1 
 
     # Trying to build the Hamiltonian
     os = OpSum()
+
     for j=1:N-1
-        os += - J,"b",j,"b dag",j+1
-        os += - J,"b dag",j,"b",j+1
-        os += U/2,"b dag", "b", j,("b dag", "b", j, -1)
+        os += - J,"A",j,"Adag",j+1
+        os += - J,"Adag",j,"A",j+1
+        os += U/2,"Adag", "A", j,("Adag", "A", j, -1)
     end
     H = MPO(os,sites)
 
-    psi0 = random_mps(sites;linkdims=10)
+    "psi0 = random_mps(sites;linkdims=10)
 
     nsweeps = 5
     maxdim = [10,20,100,100,200]
     cutoff = [1E-10]
 
-    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
+    energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)"
 
     return
 
